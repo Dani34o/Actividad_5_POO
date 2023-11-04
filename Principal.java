d403793cde6b2bb28dbd757f55e996f6a4c63b8b
Dani34o <119269053+Dani34o@users.noreply.github.com>
2023-11-03 22:40:10 -0500

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,16 @@
+package Actividad_5;
+
+
+public class Principal {
+	    
+	    public static void main(String[] args) {
+	        VentanaPrincipal miVentanaPrincipal;
+	        miVentanaPrincipal= new VentanaPrincipal();
+	        miVentanaPrincipal.setVisible(true);
+	    }
+	    
+	}
+
+	
+
+
